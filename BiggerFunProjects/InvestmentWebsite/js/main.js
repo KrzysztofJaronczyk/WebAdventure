5b2c6de536e24cfdd84447d7882ede024ae86bf5
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-01-09 11:46:10 +0100

    Fix nav handling and add required listener

--- BiggerFunProjects/InvestmentWebsite/js/main.js
+++ BiggerFunProjects/InvestmentWebsite/js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+// const navLinks = document.querySelectorAll('.nav__link')
 const footerYear = document.querySelector('.footer__year')
+
+addEventListener('click', e => {
+	if (e.target.classList.contains('nav__link')) {
+		navMobile.classList.remove('nav-mobile--active')
+		navBtn.classList.remove('is-active')
+	}
+})
 
 const handleNav = () => {
 	navMobile.classList.toggle('nav-mobile--active')
-	navBtn.classList.toggle('hamburger--active')
+	navBtn.classList.toggle('is-active')
 }
 
 navBtn.addEventListener('click', handleNav)
