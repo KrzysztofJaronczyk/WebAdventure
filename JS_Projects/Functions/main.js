7f792e2131f03320549a70237307f44d8c54c238
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-01-18 14:18:29 +0100

    Add finished functions stasks

---
+++ JS_Projects/Functions/main.js
@@ -0,0 +1,51 @@
+// const add = (x = 3, y = 7) => x + y
+
+// add()
+
+//
+
+// let score
+
+// function add(x, y) {
+// 	score = x + y
+// 	score % 2 === 0 ? even(score) : odd(score)
+// }
+
+// function even(item) {
+// 	console.log(`Number ${item} is even.`)
+// }
+
+// function odd(item) {
+// 	console.log(`Number ${item} is even.`)
+// }
+
+// add(20, 40)
+
+// let temp
+
+// function fahrenheit(x) {
+// 	temp = x * 1.8 + 32
+
+// 	console.log(`${x} celcius is equal to ${temp} fahrenheit`)
+// }
+
+// fahrenheit(20)
+
+//
+
+// const num = 10
+// const numbers = []
+
+// for (let i = 0; i < num; i++) {
+// 	numbers[i] = i
+// }
+
+// function idk(x) {
+// 	// if (x % 3 === 0) console.log(`${x} is divided by 3.`)
+// 	// else
+// 	// console.log(`${x} is not divided by 3 or ${x} = 0`)
+
+// 	x % 3 === 0 && x !== 0 ? console.log(`${x} is divided by 3.`) : console.log(`${x} is not divided by 3 or ${x} = 0`)
+// }
+
+// numbers.forEach(idk)
