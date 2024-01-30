06687e59f2e37fd9798ab674f628ebad881a6186
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-01-30 21:12:02 +0100

    Update the script

--- JavaScript/Projects/ToDoList/main.js
+++ JavaScript/Projects/ToDoList/main.js
@@ -2,9 +2,8 @@ let todoInput
 let errorInfo
 let addBtn
 let ulList
-
-const addButton = document.querySelector('.btn-add')
-const mainInput = document.querySelector('.todo-input')
+let newTodo
+let div
 
 const popup = document.querySelector('.popup')
 const popupInput = document.querySelector('.popup-input')
@@ -20,12 +19,45 @@ const prepareDOMElements = () => {
 	todoInput = document.querySelector('.todo-input')
 	errorInfo = document.querySelector('.error-info')
 	addBtn = document.querySelector('.btn-add')
-	ulList = document.querySelector('.todo-list ul')
+	ulList = document.querySelector('.todolist ul')
 }
 
 const prepareDOMEvents = () => {
-	addBtn.addEventListener('click', addNewTask)
+	addBtn.addEventListener('click', addNewTodo)
 	ulList.addEventListener('click', checkClick)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		ulList.appendChild(newTodo)
+		createToolsArea()
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Enter the task!'
+	}
+}
+
+const createToolsArea = () => {
+	div = document.createElement('div')
+	div.classList.add('tools')
+	div.innerHTML = `<button class="complete"><i class="fas fa-check"></i></button>
+    <button class="edit">EDIT</button>
+    <button class="delete"><i class="fas fa-times"></i></button>`
+	newTodo.appendChild(div)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.closest('button').classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTask(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTask(e)
+	}
 }
 
 document.addEventListener('DOMContentLoaded', main)
