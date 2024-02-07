b8bab272783c260c2e593cd58e06248dc00e71fb
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-02-07 17:36:21 +0100

    Finish up form validation project

--- JavaScript/Projects/FormValidator/script.js
+++ JavaScript/Projects/FormValidator/script.js
@@ -5,7 +5,7 @@ const email = document.getElementById('email')
 
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
-const popup = document.getElementById('popup')
+const popup = document.querySelector('.popup')
 
 let dataArray = [username, password, password2, email]
 
@@ -29,6 +29,42 @@ const checkForm = input => {
 	})
 }
 
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} must be at least ${min} characters long`)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Passwords do not match')
+	}
+}
+
+const checkEmail = email => {
+	const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	if (!regEx.test(email.value)) {
+		showError(email, 'Email is not valid')
+	} else {
+		clearError(email)
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
+}
+
 const clearError = input => {
 	const formBox = input.parentElement
 	formBox.classList.remove('error')
@@ -37,6 +73,11 @@ const clearError = input => {
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
 	checkForm(dataArray)
+	checkLength(username, 3)
+	checkLength(password, 6)
+	checkPassword(password, password2)
+	checkEmail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
