74bd8633f0fe4a00c531881bbf0780698dae952a
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-03-12 14:23:35 +0100

    Some adjustments of previous projects

--- UsefullComponents/CoolTilesAnimation1/script.js
+++ UsefullComponents/CoolTilesAnimation1/script.js
@@ -2,6 +2,7 @@ let columns = Math.floor(document.documentElement.clientWidth / 50)
 let rows = Math.floor(document.documentElement.clientHeight / 50)
 
 const tiles = document.querySelector('#tiles')
+let totalTiles = 0
 
 const colors = [
 	'rgb(229,57,53)',
@@ -27,15 +28,25 @@ const createTile = i => {
 const handleOnClick = e => {
 	count = count + 1
 
+	console.log(e)
 	anime({
 		targets: '.tile',
 		backgroundColor: colors[count % (colors.length - 1)],
+		scale: [
+			{ value: 0.1, easing: 'easeOutSine', duration: 500 },
+			{ value: 1, easing: 'easeInOutQuad', duration: 1200 },
+		],
 		delay: anime.stagger(50, {
 			grid: [columns, rows],
 			from: e,
 		}),
 	})
 }
+
+let intervalId = setInterval(() => {
+	const randomPosition = Math.floor(Math.random() * totalTiles)
+	handleOnClick(randomPosition)
+}, 8000)
 
 const createTiles = quantity => {
 	Array.from(Array(quantity)).map((tile, i) => {
@@ -52,6 +63,7 @@ const createGrid = () => {
 	tiles.style.setProperty('--rows', rows)
 
 	createTiles(columns * rows)
+	totalTiles = columns * rows
 }
 
 window.onload = () => createGrid()
